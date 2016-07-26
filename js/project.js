cf72e97dc22c4183d244df98ed6c541f4d93909b
Dustinova <dustin.hood@webcollage.com>
2016-07-26 18:31:04 -0400

    Report Card 1

--- js/project.js
+++ js/project.js
@@ -4,9 +4,87 @@
 document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
 
-function clearName() {
-  document.getElementById("name").value = "";
+function clearGrade () {
+	document.getElementById("grade").value = "";
 }
-function clearGrade() {
-  document.getElementById("grade").value = "";
+
+function clearName () {
+	document.getElementById("name").value + "";
 }
+
+function showRoster () {
+	document.querySelector(".roster").classList.toggle("open");
+	document.querySelector("#roster").classList.toggle("selected");
+	document.querySelector(".assign").classList.remove("open");
+	document.querySelector("#assign").classList.remove("selected");
+}
+
+document.getElementById("roster").onclick = showRoster;
+
+function showInputs () {
+	document.querySelector(".assign").classList.toggle("open");
+	document.querySelector("#assign").classList.toggle("selected");
+	document.querySelector(".roster").classList.remove("open");
+	document.querySelector("#roster").classList.remove("selected");
+}
+
+document.getElementById("assign").onclick = showInputs;
+
+//function copy () {
+	//document.getElementById.("reportcard").innerHTML = 
+//}
+
+
+//document.getElementById("name").onclick = addNameGrade;
+
+//document.getElementById("grade").onclick = addNameGrade;
+
+function addNameGrade () {
+	
+	var student = document.getElementById("Name").value;
+	var grade = document.getElementById("Grade").value;
+
+	if (grade >= 90) {
+		grade = "A+";
+	}
+	else if (grade >= 85) {
+		grade = "A-";
+	}
+	else if (grade >= 80) {
+		grade = "B+";
+	}
+	else if (grade >= 75) { 
+		grade = "B-";
+	}
+	else if (grade >= 70) {
+		grade = "C+";
+	}
+	else if (grade >= 65) {
+		grade = "C-";
+	}
+	else if (grade >= 60 ) {
+		grade = "D+";
+	}
+	else if (grade >= 55) {
+		grade = "D-";
+	}
+	else if (grade >= 0) {
+		grade = "F";
+	}
+
+} 
+
+document.getElementById("name").onclick = addNameGrade;
+
+document.getElementById("grade").onclick = addNameGrade;
+
+//function clearName() {
+  //document.getElementById("name").value = "Joe";
+//}
+
+//function clearGrade() {
+  //document.getElementById("grade").value = "72";
+//}
+
+
+//document.getElementById("target")somelist.innerHTML = message;
